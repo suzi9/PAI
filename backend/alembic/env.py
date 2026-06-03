25e7fc688718b15ab7b666303f0831042ee77e52
suzi9 <zuzannakre099@gmail.com>
2026-06-03 16:59:00 +0200

    final env alembic

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -1,32 +1,38 @@
 from logging.config import fileConfig
 
 from alembic import context
-from sqlalchemy import engine_from_config
+from sqlalchemy import engine_from_config, pool
 
 from app.config import settings
+from app.db import Base
 
 config = context.config
 
 if config.config_file_name:
     fileConfig(config.config_file_name)
 
-sync_url = settings.DATABASE_URL  
+sync_url = settings.DATABASE_URL.replace("asyncpg", "")
+
+target_metadata = Base.metadata
 
 def run_migrations_offline():
+    url = sync_url
     context.configure(
-        url=sync_url,
-        literal_binds=False,  
+        url=url,
+        target_metadata=target_metadata,
+        literal_binds=True,
     )
     context.run_migrations()
 
 def run_migrations_online():
     connectable = engine_from_config(
         config.get_section(config.config_ini_section),
-        prefix="sqlalchemy."
+        prefix="sqlalchemy.",
+        poolclass=pool.NullPool
     )
-
-    context.configure(connection=connectable)  
-    context.run_migrations()  
+    connection = connectable.connect()
+    context.configure(connection=connection, target_metadata=target_metadata)
+    context.run_migrations()
 
 if context.is_offline_mode():
     run_migrations_offline()
