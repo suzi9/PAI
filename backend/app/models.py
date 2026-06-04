60b9c7a9209002294139e4f8e91c19b38f644231
suzi9 <zuzannakre099@gmail.com>
2026-06-04 17:23:00 +0200

    fix on Uzytkownik class

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,9 +1,13 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey, Boolean, DateTime
+from sqlalchemy.orm import relationship
 from app.db import Base
 
 class Uzytkownik(Base):
     __tablename__ = "uzytkownicy"
 
     id = Column(Integer, primary_key=True)
-    email = Column(String)
-    haslo_hash = Column(String)
+    email = Column(String(255), unique=True)
+    haslo_hash = Column(String(255))
+    imie_nazwisko = Column(String(255))
+
+    rezerwacje = relationship("Rezerwacja")
