662dd34606b8004797ac33452b8b351c838dcec8
suzi9 <zuzannakre099@gmail.com>
2026-06-06 07:52:00 +0200

    new class UzytkownikLogowanie

--- backend/app/schemas.py
+++ backend/app/schemas.py
@@ -7,3 +7,8 @@ class UzytkownikRejestracja(BaseModel):
     email: EmailStr
     haslo: str = Field(min_length=6, max_length=128)
     imie_nazwisko: str = Field(min_length=2, max_length=255)
+
+
+class UzytkownikLogowanie(BaseModel):
+    email: EmailStr
+    haslo: str
