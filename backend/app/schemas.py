042b16578d39ef6efec976d94baf93558522ce93
suzi9 <zuzannakre099@gmail.com>
2026-06-06 07:47:00 +0200

    adding new class UzytkownikRejestracja

---
+++ backend/app/schemas.py
@@ -0,0 +1,9 @@
+from datetime import datetime
+from typing import Literal
+
+from pydantic import BaseModel, ConfigDict, EmailStr, Field
+
+class UzytkownikRejestracja(BaseModel):
+    email: EmailStr
+    haslo: str = Field(min_length=6, max_length=128)
+    imie_nazwisko: str = Field(min_length=2, max_length=255)
